9df6e2a08533d257f102d0cfacf0bef6b3db70f6
Megan Edwards <megmyfedwards@gmail.com>
2019-04-18 21:30:13 +0200

    Exercise 1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const list = document.querySelector('.news');
 const data = [
   {
     title: 'Asteroids 101',
@@ -22,3 +23,26 @@ const data = [
     image: 'https://via.placeholder.com/200x100'
   }
 ];
+
+const printNews = array => {
+  for (const item of array) {
+    const newListItem = document.createElement('li');
+    newListItem.classList.add('news__item');
+    const newListTitle = document.createElement('h2');
+    newListTitle.classList.add('news__title');
+    const newListTitleContent = document.createTextNode(item.title);
+    const newListImage = document.createElement('img');
+    newListImage.classList.add('news__image');
+    newListImage.src = item.image;
+    newListImage.alt = item.title;
+
+    newListTitle.appendChild(newListTitleContent);
+    newListItem.appendChild(newListTitle);
+    newListItem.appendChild(newListImage);
+
+    list.appendChild(newListItem);
+  }
+};
+
+printNews(data);
+
