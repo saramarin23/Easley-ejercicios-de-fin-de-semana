04bb1aed7017b38d35afc6f40d75f70074e27ae1
anniavdc <anva1189@gmail.com>
2019-04-20 01:05:04 +0200

    08-Buceando entre naves

--- js/main.js
+++ js/main.js
@@ -24,6 +24,8 @@ const container =  document.querySelector('.colors');
 //   });
 
 //06.El retorno de las paletas
+//Ahora que sabemos pintar paletas vamos a ver las paletas de las mejores naves de ciencia ficción, en esta URL https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palettes.json tenemos a las mejores: ¡¡¡Píntalas todas!!
+
 fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palettes.json')
   .then(response => response.json())
   .then(colorsData => {
@@ -60,11 +62,12 @@ function createColors(colorArr, parent){
     oneColor.setAttribute('class', 'color__item');
     oneColor.setAttribute('style', `background-color:#${colorArr[i]}`);
     parent.appendChild(oneColor);
-    
+
   }
 }
 
 //07.Mis paletas preferidas
+//Una vez las tengas todas pintadas vamos a permitir que la usuaria seleccione sus paletas preferidas. Al hacer clic en cualquiera de ellas tenemos que añadirle alguna clase que la marque como seleccionada. Como hay mucha gente a la que le cuesta decidir dejaremos que marquen como preferidas tantas paletas como quieran.
 function selectedEl(event){
   const selected = event.currentTarget;
   selected.classList.toggle('selected');
@@ -75,3 +78,26 @@ function handlerClick(palArr){
     palArr[i].addEventListener('click', selectedEl);
   }
 }
+
+//8.Ahora vamos a dejar que la usuaria filtre por nombre de nave, para ello tenemos que añadir una caja de búsqueda. Cada vez que se escriba una letra filtraremos. Si la usuaria escribe 'a' solo pintaremos las naves cuyo nombre contengan 'a', si escribe 'ac' as que contengan 'ac'. Y así!
+const inputEl = document.querySelector('#name');
+
+
+function handlerKey(){
+  search();
+}
+
+function search(){
+  const searchValue = inputEl.value;
+  const palArr = document.querySelectorAll('.item');
+  for(let i = 0; i < palArr.length; i++){
+    const title = palArr[i].querySelector('h1').innerHTML;
+    
+    if(!title.includes(searchValue)){
+      palArr[i].classList.add('hidden');
+    }else{
+      palArr[i].classList.remove('hidden');
+    }
+  }
+}
+inputEl.addEventListener('keyup', handlerKey);
