5cd7b70e30b0f8ab0e197526f7c908f7c8543f5a
anniavdc <anva1189@gmail.com>
2019-04-19 23:39:16 +0200

    06-El retorno de las paletas

--- js/main.js
+++ js/main.js
@@ -1,23 +1,64 @@
 'use strict';
 
-const container =  document.querySelector('body');
+//05.Todo es color entre tu y tus arrays
+const container =  document.querySelector('.colors');
 
-fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palette.json')
+// fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palette.json')
+//   .then(response => response.json())
+//   .then(colorsData => {
+//     const colorsObject = colorsData.palettes;
+//     const colorsArray = colorsObject[0];
+//     console.log(colorsArray);
+//     const title = document.createElement('h2');
+//     title.innerHTML = colorsArray.name;
+//     container.appendChild(title);
+
+//     const colors = colorsArray.colors;
+//     for(let i = 0; i<colors.length; i++){
+//       const oneColor = document.createElement('div');
+//       oneColor.setAttribute('class', 'color__item');
+
+//       container.appendChild(oneColor);
+//       oneColor.setAttribute('style', `background-color:#${colors[i]}`);
+//     }
+//   });
+
+//06.El retorno de las paletas
+fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palettes.json')
   .then(response => response.json())
   .then(colorsData => {
-    const colorsObject = colorsData.palettes;
-    const colorsArray = colorsObject[0];
-    console.log(colorsArray);
-    const title = document.createElement('h2');
-    title.innerHTML = colorsArray.name;
-    container.appendChild(title);
+    console.log(colorsData);
+    const palObject = colorsData.palettes;
+    console.log(palObject);
 
-    const colors = colorsArray.colors;
-    for(let i = 0; i<colors.length; i++){
-      const oneColor = document.createElement('div');
-      oneColor.setAttribute('class', 'color__item');
+    for(let i = 0; i < palObject.length; i++){
+      console.log(palObject[i]);
 
-      container.appendChild(oneColor);
-      oneColor.setAttribute('style', `background-color:#${colors[i]}`);
+      const list = document.createElement('li');
+      list.setAttribute('class', 'item');
+      container.appendChild(list);
+
+      const title = document.createElement('h1');
+      title.innerHTML = palObject[i].name;
+      list.appendChild(title);
+
+      const subtitle = document.createElement('h2');
+      subtitle.innerHTML = palObject[i].from;
+      list.appendChild(subtitle);
+
+      const colorArr = palObject[i].colors;
+      createColors(colorArr, list);
     }
+
+
   });
+
+function createColors(colorArr, parent){
+  for (let i = 0; i < colorArr.length; i++) {
+    const oneColor = document.createElement('div');
+    oneColor.setAttribute('class', 'color__item');
+    oneColor.setAttribute('style', `background-color:#${colorArr[i]}`);
+    parent.appendChild(oneColor);
+    
+  }
+}
