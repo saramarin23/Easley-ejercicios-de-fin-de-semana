6003f16f43ca9813306b227a30f1851166be9b57
anniavdc <anva1189@gmail.com>
2019-04-19 20:07:59 +0200

    02-Marte el Planeta Rojo

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ const data = [
     image: 'https://via.placeholder.com/200x100'
   },
   {
-    title: 'Humans aren\'t real',
+    title: "Humans aren't real",
     image: 'https://via.placeholder.com/200x100'
   },
   {
@@ -22,15 +22,17 @@ const data = [
     image: 'https://via.placeholder.com/200x100'
   }
 ];
+
+//1. Listado de noticias
 //Recoger de HTML los elementos
 const container = document.querySelector('.news');
 
-for(let i=0 ; i<data.length; i++){
+for (let i = 0; i < data.length; i++) {
   //Crear un li por cada elemento del array que tenga clase news__item y contenga:
-  const item= document.createElement('li');
+  const item = document.createElement('li');
   item.setAttribute('class', 'news__item');
   //titulo h2 clase news__title
-  const title =  document.createElement('h2');
+  const title = document.createElement('h2');
   title.setAttribute('class', 'news__title');
   const titleText = document.createTextNode(data[i].title);
   title.appendChild(titleText);
@@ -46,3 +48,22 @@ for(let i=0 ; i<data.length; i++){
   container.appendChild(item);
 }
 
+//2. Marte el Planeta Rojo
+function addTitleClass() {
+  const list = document.querySelectorAll('.news__item');
+  console.log(list);
+
+  for (let i = 0; i < list.length; i++) {
+    //Buscaremos todos los elementos con clase `.news__item`
+    const listTitle = list[i].querySelector('.news__title');
+    //Recorreremos la lista de elementos almacenando en una variable el contenido del título de cada elemento.
+    const titleContent = listTitle.innerHTML;
+    console.log(listTitle);
+    //Usando el método `includes()` comprobaremos si contienen “Mars” o “Martian” y aplicaremos al `li`correspondiente la clase `.news__item--from-mars`
+    if (titleContent.includes('Mars') || titleContent.includes('Martians')) {
+      list[i].setAttribute('class', 'news__item--from-mars');
+    }
+  }
+}
+
+addTitleClass();
