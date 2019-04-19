25a0b730d6aed1bce787546e6afcdd29bca2b56a
Megan Edwards <megmyfedwards@gmail.com>
2019-04-19 16:40:26 +0200

    Exercise 3

--- Ejercicio 3/js/main.js
+++ Ejercicio 3/js/main.js
@@ -1,30 +1,31 @@
 'use strict';
 
 const list = document.querySelector('.news');
-const data = [
-  {
-    title: 'Asteroids 101',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Life on Mars',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Martians invade Earth',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Humans aren\'t real',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Space The Final Frontier',
-    image: 'https://via.placeholder.com/200x100'
-  }
-];
-
 const searchTerms = ['Mars', 'Martian'];
+
+// const data = [
+//   {
+//     title: 'Asteroids 101',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Life on Mars',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Martians invade Earth',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Humans aren\'t real',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Space The Final Frontier',
+//     image: 'https://via.placeholder.com/200x100'
+//   }
+// ];
+
 
 const printNews = array => {
   for (const item of array) {
@@ -46,13 +47,10 @@ const printNews = array => {
   }
 };
 
-printNews(data);
-
 const searchTitles = (listItems, titles, strings) => {
   for (let i = 0; i < titles.length; i++) {
     for (const term of strings) {
       if (titles[i].toLowerCase().includes(term.toLowerCase())) {
-        console.log(listItems[i]);
         listItems[i].classList.add('news__item--from-mars');
       }
     }
@@ -61,7 +59,6 @@ const searchTitles = (listItems, titles, strings) => {
 
 const getTitles = () =>  {
   const allNews = document.querySelectorAll('.news__item');
-  console.log("allnews",allNews);
   let titlesList = [];
   for (let i = 0; i < allNews.length; i++) {
     titlesList.push(allNews[i].childNodes[0].innerHTML);
@@ -69,4 +66,16 @@ const getTitles = () =>  {
   searchTitles(allNews, titlesList, searchTerms);
 };
 
-getTitles();
+const getNews = () => {
+  fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/news.json')
+    .then(newsResponse => newsResponse.json())
+    .then(newsData => {
+      const data = newsData.news;
+      console.log(data);
+      printNews(data);
+      getTitles();
+    });
+};
+
+getNews();
+
