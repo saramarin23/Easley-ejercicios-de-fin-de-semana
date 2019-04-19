0e2a766c9e5defe0a4f476ed1995229df477ba0f
veroena <veronica.enamorado@gmail.com>
2019-04-19 20:49:37 +0200

    Ejercicio 1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const news = document.querySelector('.news');
 const data = [
   {
     title: 'Asteroids 101',
@@ -22,3 +23,34 @@ const data = [
     image: 'https://via.placeholder.com/200x100'
   }
 ];
+
+const paintNews = (array) => {
+ for (const item of array) {
+   //creo elementos
+   const newLi = document.createElement('li');
+   const newTitle = document.createElement('h2');
+   const newImg = document.createElement('img');
+
+   //creo contenidos
+   const newTitleContent = document.createTextNode(item.title);
+
+   //agrego contenido a elementos
+   newTitle.appendChild(newTitleContent);
+   newImg.src = item.image;
+   newImg.alt = item.title;
+
+   //agrego clases a los elementos
+   newLi.classList.add('news__item');
+   newTitle.classList.add('news__title');
+   newImg.classList.add('news__image');
+
+   //meto elementos dentro de otros
+   newLi.appendChild(newTitle);
+   newLi.appendChild(newImg);
+   
+   //añado elementos a la página
+   news.appendChild(newLi);
+ }
+}
+
+paintNews(data);
