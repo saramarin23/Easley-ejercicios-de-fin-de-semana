e333f7dc9c6d1a499468ec48a3b1de1977faed49
superanika <anamaria.romerogarcia@gmail.com>
2019-04-19 11:14:51 +0200

    exercise 1

--- js/main.js
+++ js/main.js
@@ -22,3 +22,25 @@ const data = [
     image: 'https://via.placeholder.com/200x100'
   }
 ];
+
+function createLi() {
+
+  for (const obj of data) {
+    const news = document.querySelector('.news');
+    const newItem = document.createElement('li');
+    newItem.classList.add('news__item');
+    const newTitle = document.createElement('h2');
+    newTitle.classList.add('news__title');
+    const  titleContent = document.createTextNode(`Title: ${obj.title}`);
+    const newImg = document.createElement('img');
+    newImg.classList.add('news__image');
+    newImg.src = obj.image;
+    newImg.alt = obj.title;
+    newTitle.appendChild(titleContent);
+    newItem.appendChild(newTitle);
+    newItem.appendChild(newImg);
+    news.appendChild(newItem);
+  }
+}
+
+createLi();
