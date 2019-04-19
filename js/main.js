dd5665d3e5219cd3ef10e48879618fd57f2985c1
sandrapinazo <sandrapinazogonzalez@gmail.com>
2019-04-19 21:25:54 +0200

    Ejercicio 1 - 2 variantes

--- js/main.js
+++ js/main.js
@@ -22,3 +22,36 @@ const data = [
     image: 'https://via.placeholder.com/200x100'
   }
 ];
+
+const ulEl = document.querySelector('.news');
+
+
+//Añadir contenido usando innerHTML
+
+// for (const news of data) {
+//   ulEl.innerHTML+= `
+//   <li class="news__item">
+//       <h2 class="news__title">${news.title}</h2>
+//       <img class="news__image" src="${news.image}" alt="${news.title}">
+//   </li>
+//   `;
+// }
+
+//Añadir contenido usando DOM avanzado
+
+for (const news of data) {
+  const newLi = document.createElement('li');
+  const newH2 = document.createElement('h2');
+  const newImg = document.createElement('img');
+  const title = document.createTextNode (news.title);
+  newH2.appendChild(title);
+  newH2.classList.add('news__title');
+  newImg.classList.add('news__image');
+  newLi.classList.add('news__item');
+  newImg.src = news.image;
+  newImg.alt = news.title;
+  newLi.appendChild(newH2);
+  newLi.appendChild(newImg);
+  ulEl.appendChild(newLi);
+}
+
