01a0a3cf88f458f2c25fdfa8676f4e7e2d5c7b77
anniavdc <anva1189@gmail.com>
2019-04-19 15:57:04 +0200

    01-Listado de noticias

--- js/main.js
+++ js/main.js
@@ -22,3 +22,27 @@ const data = [
     image: 'https://via.placeholder.com/200x100'
   }
 ];
+//Recoger de HTML los elementos
+const container = document.querySelector('.news');
+
+for(let i=0 ; i<data.length; i++){
+  //Crear un li por cada elemento del array que tenga clase news__item y contenga:
+  const item= document.createElement('li');
+  item.setAttribute('class', 'news__item');
+  //titulo h2 clase news__title
+  const title =  document.createElement('h2');
+  title.setAttribute('class', 'news__title');
+  const titleText = document.createTextNode(data[i].title);
+  title.appendChild(titleText);
+  //imagen news__image
+  const image = document.createElement('img');
+  image.src = data[i].image;
+  image.setAttribute('class', 'news__image');
+  image.setAttribute('alt', data[i].title);
+
+  //put img and h2 inside li and li inside ul
+  item.appendChild(title);
+  item.appendChild(image);
+  container.appendChild(item);
+}
+
