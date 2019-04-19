0fd976039ff6764b747d83904ef353e436e3beb6
sandrapinazo <sandrapinazogonzalez@gmail.com>
2019-04-19 23:39:14 +0200

    Ejercicio 3 - version 2

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 'use strict';
 
-const data = [
-  {
-    title: 'Asteroids 101',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Life on Mars',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Martians invade Earth',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Humans aren\'t real',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Space The Final Frontier',
-    image: 'https://via.placeholder.com/200x100'
-  }
-];
+// const data = [
+//   {
+//     title: 'Asteroids 101',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Life on Mars',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Martians invade Earth',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Humans aren\'t real',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Space The Final Frontier',
+//     image: 'https://via.placeholder.com/200x100'
+//   }
+// ];
 
 const ulEl = document.querySelector('.news');
 
@@ -39,35 +39,38 @@ const ulEl = document.querySelector('.news');
 
 //Añadir contenido usando DOM avanzado
 
-// for (const news of data) {
-//   const newLi = document.createElement('li');
-//   const newH2 = document.createElement('h2');
-//   const newImg = document.createElement('img');
-//   const title = document.createTextNode (news.title);
-//   newH2.appendChild(title);
-//   newH2.classList.add('news__title');
-//   newImg.classList.add('news__image');
-//   newLi.classList.add('news__item');
-//   newImg.src = news.image;
-//   newImg.alt = news.title;
-//   newLi.appendChild(newH2);
-//   newLi.appendChild(newImg);
-//   ulEl.appendChild(newLi);
-// }
+function paintNews(allNews) {
+  for (const news of allNews) {
+    const newLi = document.createElement('li');
+    const newH2 = document.createElement('h2');
+    const newImg = document.createElement('img');
+    const title = document.createTextNode (news.title);
+    newH2.appendChild(title);
+    newH2.classList.add('news__title');
+    newImg.classList.add('news__image');
+    newLi.classList.add('news__item');
+    newImg.src = news.image;
+    newImg.alt = news.title;
+    newLi.appendChild(newH2);
+    newLi.appendChild(newImg);
+    ulEl.appendChild(newLi);
+  }
+}
 
 
-// EJERCICIO 2
+//EJERCICIO 2
 
-// const newsEl = document.querySelectorAll('.news__item');
-
-// for (const news of newsEl) {
-//   const titleEl = news.querySelector('h2').innerHTML;
-//   const containsMars = titleEl.includes('Mars');
-//   const containsMartians = titleEl.includes('Martians');
-//   if (containsMars || containsMartians) {
-//     news.classList.add('news__item--from-mars');
-//   }
-// }
+function martianNews() {
+  const newsEl = document.querySelectorAll('.news__item');
+  for (const news of newsEl) {
+    const titleEl = news.querySelector('h2').innerHTML;
+    const containsMars = titleEl.includes('Mars');
+    const containsMartians = titleEl.includes('Martians');
+    if (containsMars || containsMartians) {
+      news.classList.add('news__item--from-mars');
+    }
+  }
+}
 
 
 //Ejercicio 3
@@ -76,29 +79,7 @@ fetch ('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-sem
   .then(response => response.json())
   .then(function (data){
     const {news:allNews} = data;
-    for (const news of allNews) {
-      const newLi = document.createElement('li');
-      const newH2 = document.createElement('h2');
-      const newImg = document.createElement('img');
-      const title = document.createTextNode (news.title);
-      newH2.appendChild(title);
-      newH2.classList.add('news__title');
-      newImg.classList.add('news__image');
-      newLi.classList.add('news__item');
-      newImg.src = news.image;
-      newImg.alt = news.title;
-      newLi.appendChild(newH2);
-      newLi.appendChild(newImg);
-      ulEl.appendChild(newLi);
-    }
-    const newsEl = document.querySelectorAll('.news__item');
-    for (const news of newsEl) {
-      const titleEl = news.querySelector('h2').innerHTML;
-      const containsMars = titleEl.includes('Mars');
-      const containsMartians = titleEl.includes('Martians');
-      if (containsMars || containsMartians) {
-        news.classList.add('news__item--from-mars');
-      }
-    }
+    paintNews(allNews);
+    martianNews();
   });
 
