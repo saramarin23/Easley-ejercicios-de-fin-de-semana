34136ca4e78806e07823291abefeb019a32a2062
sandrapinazo <sandrapinazogonzalez@gmail.com>
2019-04-20 03:53:42 +0200

    Ejercicio 8 - Fix select function, clean js

--- js/main.js
+++ js/main.js
@@ -1,137 +1,10 @@
 'use strict';
 
-// const data = [
-//   {
-//     title: 'Asteroids 101',
-//     image: 'https://via.placeholder.com/200x100'
-//   },
-//   {
-//     title: 'Life on Mars',
-//     image: 'https://via.placeholder.com/200x100'
-//   },
-//   {
-//     title: 'Martians invade Earth',
-//     image: 'https://via.placeholder.com/200x100'
-//   },
-//   {
-//     title: 'Humans aren\'t real',
-//     image: 'https://via.placeholder.com/200x100'
-//   },
-//   {
-//     title: 'Space The Final Frontier',
-//     image: 'https://via.placeholder.com/200x100'
-//   }
-// ];
+const inputEl = document.querySelector('.search__input');
+const palettesUlEl = document.querySelector('.palettes');
 
-const ulEl = document.querySelector('.news');
-
-//EJERCICIO 1
-//Añadir contenido usando innerHTML
-
-// for (const news of data) {
-//   ulEl.innerHTML+= `
-//   <li class="news__item">
-//       <h2 class="news__title">${news.title}</h2>
-//       <img class="news__image" src="${news.image}" alt="${news.title}">
-//   </li>
-//   `;
-// }
-
-//Añadir contenido usando DOM avanzado
-
-// function paintNews(allNews) {
-//   for (const news of allNews) {
-//     const newLi = document.createElement('li');
-//     const newH2 = document.createElement('h2');
-//     const newImg = document.createElement('img');
-//     const title = document.createTextNode (news.title);
-//     newH2.appendChild(title);
-//     newH2.classList.add('news__title');
-//     newImg.classList.add('news__image');
-//     newLi.classList.add('news__item');
-//     newImg.src = news.image;
-//     newImg.alt = news.title;
-//     newLi.appendChild(newH2);
-//     newLi.appendChild(newImg);
-//     ulEl.appendChild(newLi);
-//   }
-// }
-
-
-//EJERCICIO 2
-
-function martianNews() {
-  const newsEl = document.querySelectorAll('.news__item');
-  for (const news of newsEl) {
-    const titleEl = news.querySelector('h2').innerHTML;
-    const containsMars = titleEl.includes('Mars');
-    const containsMartians = titleEl.includes('Martians');
-    if (containsMars || containsMartians) {
-      news.classList.add('news__item--from-mars');
-    }
-  }
-}
-
-
-//EJERCICIO 3
-
-fetch ('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/news.json')
-  .then(response => response.json())
-  .then(function (data){
-    const {news:allNews} = data;
-    paintNews(allNews);
-    martianNews();
-    addListenersToLi(); // para el ej4, función definida más adelante
-  });
-
-
-//EJERCICIO 4
-//modifico la función paintNews para que también añada la clase que oculta las imagenes al crear los 'li'.
-
-function paintNews(allNews) {
-  for (const news of allNews) {
-    const newLi = document.createElement('li');
-    const newH2 = document.createElement('h2');
-    const newImg = document.createElement('img');
-    const title = document.createTextNode (news.title);
-    newH2.appendChild(title);
-    newH2.classList.add('news__title');
-    newImg.classList.add('news__image');
-    newLi.classList.add('news__item');
-    newLi.classList.add('news__item--no-image-visible');
-    newImg.src = news.image;
-    newImg.alt = news.title;
-    newLi.appendChild(newH2);
-    newLi.appendChild(newImg);
-    ulEl.appendChild(newLi);
-  }
-}
-
-//para añadir listeners a todos los 'li' que se creen
-function addListenersToLi () {
-  const newsEl = document.querySelectorAll('.news__item');
-  for (const news of newsEl){
-    news.addEventListener('click', toggleImage);
-  }
-}
-
-function toggleImage (event) {
-  const currentNews = event.currentTarget;
-  currentNews.classList.toggle('news__item--no-image-visible');
-}
-
-//EJERCICIO 5
-
-// fetch ('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palette.json')
-//   .then(response => response.json())
-//   .then(function(data){
-//     const {palettes} = data;
-//     const [ship] = palettes;
-//     createPalette(ship);
-//   });
 
 function createPalette (ship) {
-  const palettesUlEl = document.querySelector('.palettes');
   const newLi = document.createElement('li');
   const newH2 = document.createElement('h2');
   newLi.classList.add('palette__container');
@@ -145,21 +18,6 @@ function createPalette (ship) {
   }
 }
 
-//EJERCICIO 6
-
-// fetch ('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palettes.json')
-//   .then(response => response.json())
-//   .then(function(data){
-//     const {palettes} = data;
-//     for (const ship of palettes){
-//       createPalette(ship);
-      
-//     }
-//     addListenersToPaletteContainers(); //añadido para ejercicio 7
-//   });
-
-//EJERCICIO 7
-
 function addListenersToPaletteContainers () {
   const paletteContainers = document.querySelectorAll('.palette__container');
   for (const container of paletteContainers){
@@ -170,10 +28,6 @@ function addListenersToPaletteContainers () {
 function selectPalette (event) {
   event.currentTarget.classList.toggle('selected__palette');
 }
-
-//EJERCICIO 8
-
-const inputEl = document.querySelector('.search__input');
 
 fetch ('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palettes.json')
   .then(response => response.json())
@@ -193,7 +47,8 @@ fetch ('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-sem
           createPalette(ship);
         }
       }
+      addListenersToPaletteContainers();
     }
-    addListenersToPaletteContainers(); //añadido para ejercicio 7
     inputEl.addEventListener('keyup', searchShip);
+    addListenersToPaletteContainers();
   });
