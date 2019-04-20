55b1f25e1dbc4bd6a1acac4d95c8c68142a8ba04
libertadpozos <libertad.pozos@gmail.com>
2019-04-20 12:29:40 +0200

    Excercise 1

--- js/main.js
+++ js/main.js
@@ -22,3 +22,17 @@ const data = [
     image: 'https://via.placeholder.com/200x100'
   }
 ];
+
+const ulEl = document.querySelector('.news');
+
+console.log (data[1].title);
+
+
+for (let i=0; i<data.length; i++){
+  ulEl.innerHTML+=
+  `<li class="news__item">
+      <h2 class="news__title"> ${data[i].title}</h2>
+      <img class="news__image" src=${data[i].image}
+      alt=${data[i].title}>
+    </li>`
+}
