797ce69999eedb89e31da4b3ffea5e5641422620
sandrapinazo <sandrapinazogonzalez@gmail.com>
2019-04-20 06:06:35 +0200

    Ejercicio 9 - v2

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@
 const inputEl = document.querySelector('.search__input');
 const palettesUlEl = document.querySelector('.palettes');
 
-
 function createPalette (ship) {
   const newLi = document.createElement('li');
   const newH2 = document.createElement('h2');
@@ -18,6 +17,10 @@ function createPalette (ship) {
   }
 }
 
+function selectPalette (event) {
+  event.currentTarget.classList.toggle('selected__palette');
+}
+
 function addListenersToPaletteContainers () {
   const paletteContainers = document.querySelectorAll('.palette__container');
   for (const container of paletteContainers){
@@ -25,38 +28,76 @@ function addListenersToPaletteContainers () {
   }
 }
 
-function selectPalette (event) {
-  event.currentTarget.classList.toggle('selected__palette');
-}
+const palettesStorage = JSON.parse(localStorage.getItem('palettes'));
 
-if (localStorage.getItem('palettes')){
-  const palettes = JSON.parse(localStorage.getItem('palettes'));
-  for (const ship of palettes){
-    createPalette(ship);
-  }
-}else {
+const palettesFetch = () => 
   fetch ('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palettes.json')
     .then(response => response.json())
     .then(function(data){
       const {palettes} = data;
-      localStorage.setItem('palettes',JSON.stringify(palettes));
-      for (const ship of palettes){
-        createPalette(ship);
-      }
-      function searchShip (){
-        const searchText = inputEl.value;
-        const palettesUlEl = document.querySelector('.palettes');
-        palettesUlEl.innerHTML = '';
-        for (const ship of palettes) {
-          const search = searchText.toLowerCase();
-          const shipName = ship.name.toLowerCase();
-          if(shipName.includes(search)){
-            createPalette(ship);
-          }
+      return palettes;});
+    
+
+var promises = [palettesStorage, palettesFetch()];
+
+Promise.all(promises)
+  .then(function (promises){
+    const palettes = promises[0] || promises[1];
+    console.log(palettes, promises);
+    for (const ship of palettes){
+      createPalette(ship);
+    }
+    const paletteContainers = document.querySelectorAll('.palette__container');
+    addListenersToPaletteContainers();
+    function searchShip (){
+      const searchText = inputEl.value;
+      for (const container of paletteContainers){
+        const search = searchText.toLowerCase();
+        const shipNameUpper = container.querySelector('h2').innerHTML;
+        const shipName = shipNameUpper.toLowerCase();
+        if(shipName.includes(search)){
+          container.classList.remove('hidden');
+        }else {
+          container.classList.add('hidden');
         }
-        addListenersToPaletteContainers();
       }
-      inputEl.addEventListener('keyup', searchShip);
-      addListenersToPaletteContainers();
-    });
-}
+    }
+    inputEl.addEventListener('keyup', searchShip);
+    localStorage.setItem('palettes',JSON.stringify(palettes));
+  });
+  
+
+// console.log(palettesData);
+
+// if (localStorage.getItem('palettes')){
+//   const palettes = JSON.parse(localStorage.getItem('palettes'));
+//   for (const ship of palettes){
+//     createPalette(ship);
+//   }
+// }else {
+//   fetch ('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palettes.json')
+//     .then(response => response.json())
+//     .then(function(data){
+//       const {palettes} = data;
+//       for (const ship of palettes){
+//         createPalette(ship);
+//       }
+//       const paletteContainers = document.querySelectorAll('.palette__container');
+//       addListenersToPaletteContainers();
+//       function searchShip (){
+//         const searchText = inputEl.value;
+//         for (const container of paletteContainers){
+//           const search = searchText.toLowerCase();
+//           const shipNameUpper = container.querySelector('h2');
+//           const shipName = shipNameUpper.toLowerCase();
+//           if(shipName.includes(search)){
+//             container.classList.remove('hidden');
+//           }else {
+//             container.classList.add('hidden');
+//           }
+//         }
+//       }
+//       inputEl.addEventListener('keyup', searchShip);
+//       localStorage.setItem('palettes',JSON.stringify(palettes));
+//     });
+// }
