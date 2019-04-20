57500c1975c5029cd990a6a6d03d476e44d711ed
Ari <ariane.jurado@gmail.com>
2019-04-20 21:01:37 +0200

    finish ej 8 and 9, forgot commit ej8

--- js/main.js
+++ js/main.js
@@ -94,29 +94,188 @@
 //   }
 // });
 //-------------------------EJERCICIO 5
-const api =
-  'https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palette.json';
-fetch(api)
-  .then(response => response.json())
-  .then(data => {
+// const api =
+//   'https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palette.json';
+// fetch(api)
+//   .then(response => response.json())
+//   .then(data => {
+//     const array = data.palettes;
+
+//     for (let i = 0; i < array.length; i++) {
+//       const items = array[i];
+//       const colors = items.colors;
+//       console.log('array de colores', colors);
+
+//       for (const colorsItems of colors) {
+//         console.log('hola', colorsItems);
+//       }
+
+//       for (let i = 0; i < colors.length; i++) {
+//         const body = document.querySelector('body');
+//         const container = document.createElement('div');
+//         body.appendChild(container);
+//         container.setAttribute('class', 'color__item');
+//         container.setAttribute('style', `background-color: #${colors[i]}`);
+//         console.log('pri', colors[i]);
+//       }
+//     }
+//   });
+
+//-----------------EJERCICIO 6
+
+// const api = 'https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palettes.json';
+
+// fetch(api)
+//     .then(response => response.json())
+//     .then(data => {
+//         const array = data.palettes;
+
+//         for (let i = 0; i < array.length; i++) {
+//             const element = array[i];
+//             const colors = element.colors;
+//             console.log('holaaaaa', colors);
+//             const list = document.createElement('li');
+//             list.setAttribute('class', 'container');
+//             console.log('NAME', element.name);
+//             console.log('FROM', element.from);
+//             const name = document.createTextNode(`Name: ${element.name}`);
+//             const from = document.createTextNode(`. From: ${element.from}`);
+//             list.appendChild(name);
+//             list.appendChild(from);
+//             for (const eachColor of colors) {
+//                 console.log('chao', eachColor);
+//                 const body = document.querySelector('body');
+//                 const colorContainer = document.createElement('div');
+//                 colorContainer.setAttribute('class', 'color__item');
+//                 colorContainer.setAttribute('style', `background-color: #${eachColor}`);
+//                 list.appendChild(colorContainer);
+//                 body.appendChild(list);
+//             }
+//         }
+//     });
+//--------------------EJERCICIO 7
+
+const api = 'https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palettes.json';
+//---------------------EJERCICIO 9
+if(localStorage.getItem('array') === null) {
+    fetch(api)
+    .then(response => response.json()).then(data => {
+        processData(data)
+        localStorage.setItem('array', JSON.stringify(data))
+    });
+} else {
+    processData(JSON.parse(localStorage.getItem('array')));
+}
+
+function processData(data) {
+
     const array = data.palettes;
 
     for (let i = 0; i < array.length; i++) {
-      const items = array[i];
-      const colors = items.colors;
-      console.log('array de colores', colors);
+        const element = array[i];
+        const colors = element.colors;
 
-      for (const colorsItems of colors) {
-        console.log('hola', colorsItems);
-      }
+        const list = document.createElement('li');
+        list.setAttribute('class', 'container');
 
-      for (let i = 0; i < colors.length; i++) {
-        const body = document.querySelector('body');
-        const container = document.createElement('div');
-        body.appendChild(container);
-        container.setAttribute('class', 'color__item');
-        container.setAttribute('style', `background-color: #${colors[i]}`);
-        console.log('pri', colors[i]);
-      }
+        const elementName = element.name;
+        const name = document.createTextNode(`Name: ${element.name}`);
+        const from = document.createTextNode(`. From: ${element.from}`);
+        list.appendChild(name);
+        list.appendChild(from);
+        for (const eachColor of colors) {
+
+            const body = document.querySelector('body');
+            const colorContainer = document.createElement('div');
+            colorContainer.setAttribute('class', 'color__item');
+            colorContainer.setAttribute('style', `background-color: #${eachColor}`);
+            list.appendChild(colorContainer);
+            const ul = document.querySelector('.news');
+            ul.appendChild(list);
+        }
+            const favouriteShip = e => {
+                const trigger = e.currentTarget;
+                if (trigger.classList.contains('container')) {
+                    trigger.classList.remove('container');
+                    trigger.classList.add('selected');
+                } else {
+                    trigger.classList.add('container');
+                    trigger.classList.remove('selected');
+                }
+            }
+            list.addEventListener('click', favouriteShip);
+        //----------------EJERCCIO8
+        const input = document.querySelector('input');
+        const filter = () => {
+            // console.log(input.value);
+            if (elementName.includes(input.value)) {
+                list.classList.remove('hidden');
+                list.classList.add('container');
+            } else {
+                list.classList.remove('container');
+                list.classList.add('hidden');
+            }
+        }
+        input.addEventListener('keyup', filter);
     }
-  });
+}
+// console.log(localStorage.getItem('array'));
+// fetch(api)
+//     .then(response => response.json())
+    // .then(data => {
+        // const array = data.palettes;
+
+        // for (let i = 0; i < array.length; i++) {
+        //     const element = array[i];
+        //     const colors = element.colors;
+
+        //     const list = document.createElement('li');
+        //     list.setAttribute('class', 'container');
+
+        //     const elementName = element.name;
+        //     const name = document.createTextNode(`Name: ${element.name}`);
+        //     const from = document.createTextNode(`. From: ${element.from}`);
+        //     list.appendChild(name);
+        //     list.appendChild(from);
+        //     for (const eachColor of colors) {
+
+        //         const body = document.querySelector('body');
+        //         const colorContainer = document.createElement('div');
+        //         colorContainer.setAttribute('class', 'color__item');
+        //         colorContainer.setAttribute('style', `background-color: #${eachColor}`);
+        //         list.appendChild(colorContainer);
+        //         const ul = document.querySelector('.news');
+        //         ul.appendChild(list);
+        //     }
+        //         const favouriteShip = e => {
+        //             const trigger = e.currentTarget;
+        //             if (trigger.classList.contains('container')) {
+        //                 trigger.classList.remove('container');
+        //                 trigger.classList.add('selected');
+        //             } else {
+        //                 trigger.classList.add('container');
+        //                 trigger.classList.remove('selected');
+        //             }
+        //         }
+        //         list.addEventListener('click', favouriteShip);
+        //     //----------------EJERCCIO8
+        //     const input = document.querySelector('input');
+        //     const filter = () => {
+        //         console.log(input.value);
+        //         if (elementName.includes(input.value)) {
+        //             list.classList.remove('hidden');
+        //             list.classList.add('container');
+        //         } else {
+        //             list.classList.remove('container');
+        //             list.classList.add('hidden');
+        //         }
+        //     }
+        //     input.addEventListener('keyup', filter);
+        // }
+        //--------------EJERCICIO 9
+        
+        
+    
+
+   
+    
