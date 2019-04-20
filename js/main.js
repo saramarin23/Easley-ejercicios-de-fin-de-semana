fc76030ee7e98ff902902760808a6876d51e5055
anajd <ana.jaime.diaz@gmail.com>
2019-04-20 17:17:54 +0200

    Exercise 2 Marte, el planeta rojo

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ for (let i = 0; i < data.length; i++) {
   const newItem = document.createElement('li');
   newItem.classList.add('news__item');
   const subtitle = document.createElement('h2');
-  subtitle.classList.add('news__tittle');
+  subtitle.classList.add('news__title');
   const image = document.createElement('img');
   image.classList.add('news__image');
   image.setAttribute('src', data[i].image);
@@ -45,3 +45,16 @@ for (let i = 0; i < data.length; i++) {
   newItem.appendChild(image);
   news.appendChild(newItem);
 }
+
+// Ejercicio 2 - Marte, el planeta rojo
+const mars = () => {
+  const listNews = document.querySelectorAll('.news__item');
+  for (const item of listNews) {
+    const title = item.querySelector('.news__title').innerHTML;
+    if (title.includes('Mars') || title.includes('Martian')) {
+      item.classList.add('news__item--from-mars');
+    }
+  }
+};
+
+mars();
