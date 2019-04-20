6e68dc49bd7b9fe2e574465b0a7983e525492de3
libertadpozos <libertad.pozos@gmail.com>
2019-04-20 13:29:29 +0200

    Add excercise 2

--- js/main.js
+++ js/main.js
@@ -1,38 +1,50 @@
-'use strict';
+"use strict";
 
 const data = [
   {
-    title: 'Asteroids 101',
-    image: 'https://via.placeholder.com/200x100'
+    title: "Asteroids 101",
+    image: "https://via.placeholder.com/200x100"
   },
   {
-    title: 'Life on Mars',
-    image: 'https://via.placeholder.com/200x100'
+    title: "Life on Mars",
+    image: "https://via.placeholder.com/200x100"
   },
   {
-    title: 'Martians invade Earth',
-    image: 'https://via.placeholder.com/200x100'
+    title: "Martians invade Earth",
+    image: "https://via.placeholder.com/200x100"
   },
   {
-    title: 'Humans aren\'t real',
-    image: 'https://via.placeholder.com/200x100'
+    title: "Humans aren't real",
+    image: "https://via.placeholder.com/200x100"
   },
   {
-    title: 'Space The Final Frontier',
-    image: 'https://via.placeholder.com/200x100'
+    title: "Space The Final Frontier",
+    image: "https://via.placeholder.com/200x100"
   }
 ];
 
-const ulEl = document.querySelector('.news');
+//EJERCICIO 1
+const ulEl = document.querySelector(".news");
 
-console.log (data[1].title);
-
-
-for (let i=0; i<data.length; i++){
-  ulEl.innerHTML+=
-  `<li class="news__item">
+for (let i = 0; i < data.length; i++) {
+  ulEl.innerHTML += `<li class="news__item">
       <h2 class="news__title"> ${data[i].title}</h2>
       <img class="news__image" src=${data[i].image}
       alt=${data[i].title}>
-    </li>`
+    </li>`;
 }
+
+//EJERCICIO 2
+
+// Buscaremos todos los elementos con clase .news__item.
+const arrNewsItems = document.querySelectorAll(".news__item");
+
+for (let i = 0; i < arrNewsItems.length; i++) {
+  if (
+    arrNewsItems[i].innerText.includes("Martians") ||
+    arrNewsItems[i].innerText.includes("Mars")
+  ) {
+    arrNewsItems[i].classList.add("news__item--from-mars");
+  }
+}
+
