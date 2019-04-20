f620b9874582aef633003d52e0eb7339d293f0d7
Irene Fuente <ifuenteromero@gmail.com>
2019-04-20 20:35:40 +0200

    ej5

--- js/main.js
+++ js/main.js
@@ -22,50 +22,71 @@
 //     image: 'https://via.placeholder.com/200x100'
 //   }
 // ];
-let dataNews = [];
-const listEl = document.querySelector('.news');
-function query(){
-  fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/news.json').then(response => response.json()).then(data => {
-    dataNews = data.news;
-    addHtml();
-    addTitleStyles();
-  })
-};
-query();
+// let dataNews = [];
+// const listEl = document.querySelector('.news');
+// function query(){
+//   fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/news.json').then(response => response.json()).then(data => {
+//     dataNews = data.news;
+//     addHtml();
+//     addTitleStyles();
+//   })
+// };
+// query();
 
-function addHtml() {
-  for (const news of dataNews) {
-    const newsItem = document.createElement('ĺi');
-    const newsTitle = document.createElement('h2');
-    const newsTitleContent = document.createTextNode(news.title);
-    const newsImage = document.createElement('img');
-    newsItem.classList.add('news__item');
-    newsItem.addEventListener('click',showOrHide);
-    newsTitle.classList.add('news__title');
-    newsImage.classList.add('news__image');
-    newsImage.classList.add('news__item--no-image-visible');
-    listEl.appendChild(newsItem);
-    newsItem.appendChild(newsTitle);
-    newsItem.appendChild(newsImage);
-    newsImage.src = news.image;
-    newsImage.alt = news.title;
-    newsTitle.appendChild(newsTitleContent);
+// function addHtml() {
+//   for (const news of dataNews) {
+//     const newsItem = document.createElement('ĺi');
+//     const newsTitle = document.createElement('h2');
+//     const newsTitleContent = document.createTextNode(news.title);
+//     const newsImage = document.createElement('img');
+//     newsItem.classList.add('news__item');
+//     newsItem.addEventListener('click',showOrHide);
+//     newsTitle.classList.add('news__title');
+//     newsImage.classList.add('news__image');
+//     newsImage.classList.add('news__item--no-image-visible');
+//     listEl.appendChild(newsItem);
+//     newsItem.appendChild(newsTitle);
+//     newsItem.appendChild(newsImage);
+//     newsImage.src = news.image;
+//     newsImage.alt = news.title;
+//     newsTitle.appendChild(newsTitleContent);
 
-  }
+//   }
+// }
+// function addTitleStyles() {
+//   const titlesList = document.querySelectorAll('.news__item');
+//   for (const titleEl of titlesList) {
+//     const title = titleEl.querySelector('.news__title').innerHTML;
+//     if (title.includes('Mars')||title.includes('Martian')) {
+//       titleEl.classList.add('news__item--from-mars');
+//     }
+//   }
+// }
+
+
+// function showOrHide(event){
+//   const el=event.currentTarget;
+//   const img=el.querySelector('.news__image');
+//   img.classList.toggle('news__item--no-image-visible');
+// }
+
+
+//ejercicio5
+
+const container = document.querySelector('.container');
+const btnPaletteEl = document.querySelector('.btn');
+btnPaletteEl.addEventListener('click',getPalette);
+function getPalette(){
+  fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palette.json').then(response=>response.json())
+  .then(data=>createPalette(data))
 }
-function addTitleStyles() {
-  const titlesList = document.querySelectorAll('.news__item');
-  for (const titleEl of titlesList) {
-    const title = titleEl.querySelector('.news__title').innerHTML;
-    if (title.includes('Mars')||title.includes('Martian')) {
-      titleEl.classList.add('news__item--from-mars');
-    }
-  }
-}
 
-
-function showOrHide(event){
-  const el=event.currentTarget;
-  const img=el.querySelector('.news__image');
-  img.classList.toggle('news__item--no-image-visible');
-}
+function createPalette(hex){
+  container.innerHTML="";
+  const palette = hex.palettes[0].colors;
+  for(const color of palette){
+   const divcolor = document.createElement('div');
+   container.appendChild(divcolor);
+   divcolor.classList.add('rect');
+   divcolor.setAttribute('style','background-color:#'+color)
+}}
