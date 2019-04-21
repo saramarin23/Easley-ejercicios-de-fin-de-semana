01802061ff177fb120c3e09c697128be8f5a5731
iris paredes <irisparedesjimenez@gmail.com>
2019-04-21 12:33:46 +0200

    exercise 3

--- js/main.js
+++ js/main.js
@@ -1,67 +1,121 @@
-'use strict';
+ 'use strict';
 
-// ejercicio 1
+// // ejercicio 1
+// const list = document.querySelector('.news');
+
+// const data = [
+//   {
+//     title: 'Asteroids 101',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Life on Mars',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Martians invade Earth',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Humans aren\'t real',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Space The Final Frontier',
+//     image: 'https://via.placeholder.com/200x100'
+//   }
+// ];
+
+// function paintNews (arr) {
+//   for (const item of arr) {
+//     const newsList = document.createElement('li');
+//     newsList.classList.add('news__item');
+
+//     const newsListTitle = document.createElement('h2');
+//     newsListTitle.classList.add('news__title');
+
+//     const titleContent = document.createTextNode(item.title)
+
+//     const image = document.createElement('img');
+//     image.classList.add('news__image');
+//     image.src = item.image;
+//     image.alt = item.title;
+
+//     newsListTitle.appendChild(titleContent);
+//     newsList.appendChild(newsListTitle);
+//     newsList.appendChild(image);
+
+//     list.appendChild(newsList);
+//   }
+// };    
+// paintNews(data);
+
+// // ejercicio 2
+
+// function marsMartian () {
+//   const titleMars = document.querySelectorAll('.news__item');
+//   for (const martian of titleMars) {
+//     let titleElement = martian.querySelector('.news__title').innerHTML;
+//     if (titleElement.includes('Mars') || titleElement.includes('Martian')){
+//       martian.classList.add('news__item--from-mars');
+//     }
+//   } 
+// };
+// marsMartian();
+
+// // ejercicio 3
 const list = document.querySelector('.news');
-
 const data = [
-  {
-    title: 'Asteroids 101',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Life on Mars',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Martians invade Earth',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Humans aren\'t real',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Space The Final Frontier',
-    image: 'https://via.placeholder.com/200x100'
-  }
-];
-
-function paintNews (arr) {
-  for (const item of arr) {
-    const newsList = document.createElement('li');
-    newsList.classList.add('news__item');
-
-    const newsListTitle = document.createElement('h2');
-    newsListTitle.classList.add('news__title');
-
-    const titleContent = document.createTextNode(item.title)
-
-    const image = document.createElement('img');
-    image.classList.add('news__image');
-    image.src = item.image;
-    image.alt = item.title;
-
-    newsListTitle.appendChild(titleContent);
-    newsList.appendChild(newsListTitle);
-    newsList.appendChild(image);
-
-    list.appendChild(newsList);
-  }
-};    
-paintNews(data);
-
-// ejercicio 2
-
-function marsMartian () {
-  const titleMars = document.querySelectorAll('.news__item');
-  for (const martian of titleMars) {
-    let titleElement = martian.querySelector('.news__title').innerHTML;
-    if (titleElement.includes('Mars') || titleElement.includes('Martian')){
-      martian.classList.add('news__item--from-mars');
+    {
+      title: 'Asteroids 101',
+      image: 'https://via.placeholder.com/200x100'
+    },
+    {
+      title: 'Life on Mars',
+      image: 'https://via.placeholder.com/200x100'
+    },
+    {
+      title: 'Martians invade Earth',
+      image: 'https://via.placeholder.com/200x100'
+    },
+    {
+      title: 'Humans aren\'t real',
+      image: 'https://via.placeholder.com/200x100'
+    },
+    {
+      title: 'Space The Final Frontier',
+      image: 'https://via.placeholder.com/200x100'
     }
-  } 
-};
-marsMartian();
+  ];
+ fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/news.json')
+ .then(response => response.json())
+ .then(dataNews => {
+    const arr = dataNews.news;
+    for (const item of arr) {
+        
+        const newsList = document.createElement('li');
+        newsList.classList.add('news__item');
+    
+        const newsListTitle = document.createElement('h2');
+        newsListTitle.classList.add('news__title');
+    
+        const titleContent = document.createTextNode(item.title)
+    
+        const image = document.createElement('img');
+        image.classList.add('news__image');
+        image.src = item.image;
+        image.alt = item.title;
+    
+        newsListTitle.appendChild(titleContent);
+        newsList.appendChild(newsListTitle);
+        newsList.appendChild(image);
+    
+        list.appendChild(newsList);
+      }
+    });
+
+
+
 
 
 
