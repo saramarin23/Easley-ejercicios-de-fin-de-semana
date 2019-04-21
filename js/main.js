297b74c08d006ba46ff40bab103716670fd28d2e
libertadpozos <libertad.pozos@gmail.com>
2019-04-21 17:19:49 +0200

    Add excercise 6 corrections

--- js/main.js
+++ js/main.js
@@ -142,12 +142,15 @@ const data = [
 //  }
 //  createElements();
 
-//ejercicio 6 
+
+
+//Ejercicio 7
 // const ulEl = document.querySelector(".news");
 // function createElements(){
 //   fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palettes.json')
 //    .then(response=>response.json())
 //    .then(data=>{
+//      console.log(data);
 //      const arrPalette= data.palettes;
 //      const arrColors= arrPalette[0].colors;
 //      console.log(arrColors);  
@@ -155,6 +158,7 @@ const data = [
 //      for(let color of arrColors){
 //        //creamos nuestro li  
 //       const newItem=document.createElement('li');
+//       newItem.addEventListener('click', addSelected); //le agrego event litener a cada li.
 //       //Creamos contenido del li
 //      const newDiv=document.createElement('div');
 //      //añadimos atributos  al contenido de li
@@ -168,39 +172,108 @@ const data = [
 //    })
 //    }
 //    createElements();
+//    addSelected();
 
-//Ejercicio 7
-const ulEl = document.querySelector(".news");
-function createElements(){
+
+//    //añadir clase de selected a cada una de ellas. 
+//    function addSelected(event){
+//      const selectedItem=event.currentTarget;
+//      selectedItem.classList.toggle('selected');
+//    }
+
+
+// const ulcolors = document.querySelector(".colors");
+// function createElements(){
+//   fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palettes.json')
+//    .then(response=>response.json())
+//    .then(data=>{
+  
+//     const arrPalette= data.palettes;
+//     console.log(arrPalette);
+
+//     for (let i=0; i<arrPalette.length; i++){
+//       let arrColors = arrPalette[i].colors;
+//       console.log(arrColors);
+//       ulcolors.innerHTML+= `<li> <h2> ${arrPalette[i].name} 
+//       </h2></li>`;
+//     }
+   
+    
+//     })
+//    }
+//   createElements();
+
+  // function createTitles(){
+    
+      
+  //     const contentTitle= titles;
+  //     console.log(contentTitle);
+  //   }
+  // }
+ 
+
+
+  //    const arrColors= arrPalette[0].colors;
+  //    console.log(arrColors);  
+  // //hacemos el for
+  //    for(let color of arrColors){
+  //      //creamos nuestro li  
+  //     const newItem=document.createElement('li');
+  //     newItem.addEventListener('click', addSelected); //le agrego event litener a cada li.
+  //     //Creamos contenido del li
+  //    const newDiv=document.createElement('div');
+  //    //añadimos atributos  al contenido de li
+  //    newDiv.setAttribute('style', 'background-color:#' + color);
+  //    newDiv.setAttribute('class', 'color-container');
+  //  //se lo añadimos a nuestro Li
+  //  newItem.appendChild(newDiv);
+  //  //añadir nuestro Li con todo a UL
+  //  ulEl.appendChild(newItem);
+  // }
+  //  })
+  //  }
+  //  createElements();
+  //  addSelected();
+
+
+  //  //añadir clase de selected a cada una de ellas. 
+  //  function addSelected(event){
+  //    const selectedItem=event.currentTarget;
+  //    selectedItem.classList.toggle('selected');
+  //  }
+
+
+  //EJERCICIO 6 VERSION 2
+    const container =  document.querySelector('.colors');
   fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palettes.json')
-   .then(response=>response.json())
-   .then(data=>{
-     const arrPalette= data.palettes;
-     const arrColors= arrPalette[0].colors;
-     console.log(arrColors);  
-  //hacemos el for
-     for(let color of arrColors){
-       //creamos nuestro li  
-      const newItem=document.createElement('li');
-      newItem.addEventListener('click', addSelected); //le agrego event litener a cada li.
-      //Creamos contenido del li
-     const newDiv=document.createElement('div');
-     //añadimos atributos  al contenido de li
-     newDiv.setAttribute('style', 'background-color:#' + color);
-     newDiv.setAttribute('class', 'color-container');
-   //se lo añadimos a nuestro Li
-   newItem.appendChild(newDiv);
-   //añadir nuestro Li con todo a UL
-   ulEl.appendChild(newItem);
-  }
-   })
-   }
-   createElements();
-   addSelected();
+  .then(response => response.json())
+  .then(colorsData => {
+    const colorsObject = colorsData.palettes;
+   
+
+    for (let i=0; i<colorsObject.length; i++){
+    const colorsArray = colorsObject[i]; //primer elemento. 
+    console.log(colorsArray);
+
+    const li= document.createElement('li'); //1. creo li
+    container.appendChild(li); //el contenedor va dentro de la lista
+
+    const title = document.createElement('h2'); //1.creo el elemento
+     title.innerHTML = colorsArray.name;// 2. creo contenido del elemento
+     li.appendChild(title);//3. el elemento va adentro ded li. 
+
+     const colors = colorsArray.colors;
+     console.log(colors);
+
+     for(let i = 0; i<colors.length; i++){
+       const oneColor = document.createElement('div'); // 1. creo el elemento
+       oneColor.setAttribute('class', 'color__item'); // pongo clase
+       li.appendChild(oneColor); //el elemento va adentro de la lista. 
+       oneColor.setAttribute('style', `background-color:#${colors[i]}`); //pongo un style a mi div que es cada color de mi array de colores. 
+    
+     }
+    } 
+  })
 
 
-   //añadir clase de selected a cada una de ellas. 
-   function addSelected(event){
-     const selectedItem=event.currentTarget;
-     selectedItem.classList.toggle('selected');
-   }
+
