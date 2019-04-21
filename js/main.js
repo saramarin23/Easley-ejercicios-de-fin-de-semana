3f3c25b08eaa9068e33196edf85825a18fefb0f8
LorenaBH <lorenabenitoherrero@gmail.com>
2019-04-21 14:02:28 +0200

    done third exercise

--- js/main.js
+++ js/main.js
@@ -25,29 +25,57 @@ const data = [
   }
 ];
 
+// function renderNews () {
+//   for(const itemNew of data) {
+//     const newItem = document.createElement('li');
+//     newItem.classList.add('news__item');
+//     const newTitle = document.createElement('h2');
+//     newTitle.classList.add('news__title');
+//     const newImage = document.createElement('img');
+//     newImage.classList.add('news__image');
+//     newImage.src = itemNew.image;
+//     newImage.alt = itemNew.title;
+//     const newContentTitle = document.createTextNode(itemNew.title);
+//     newTitle.appendChild(newContentTitle);
+//     newItem.appendChild(newImage);
+//     newItem.appendChild(newTitle);
+//     contentList.appendChild(newItem);
+//     const contentTitleMars = itemNew.title;
+//       if (contentTitleMars.includes('Mars') || contentTitleMars.includes('Martians')){ 
+//         newItem.classList.add('news__item--from-mars');
+//         console.log(contentTitleMars);
+//       } 
+//   }
+//   console.log(data);
+// }
+
+// renderNews();
+
 function renderNews () {
-  for(const itemNew of data) {
-    const newItem = document.createElement('li');
-    newItem.classList.add('news__item');
-    const newTitle = document.createElement('h2');
-    newTitle.classList.add('news__title');
-    const newImage = document.createElement('img');
-    newImage.classList.add('news__image');
-    newImage.src = itemNew.image;
-    newImage.alt = itemNew.title;
-    const newContentTitle = document.createTextNode(itemNew.title);
-    newTitle.appendChild(newContentTitle);
-    newItem.appendChild(newImage);
-    newItem.appendChild(newTitle);
-    contentList.appendChild(newItem);
-    const contentTitleMars = itemNew.title;
-      if (contentTitleMars.includes('Mars') || contentTitleMars.includes('Martians')){ 
-        newItem.classList.add('news__item--from-mars');
-        console.log(contentTitleMars);
+fetch (api)
+  .then(response => response.json())
+    .then(data => {
+      for (const itemNew of data.news) {
+        const newItem = document.createElement('li');
+        newItem.classList.add('news__item');
+        const newTitle = document.createElement('h2');
+        newTitle.classList.add('news__title');
+        const newImage = document.createElement('img');
+        newImage.classList.add('news__image');
+        newImage.src = itemNew.image;
+        newImage.alt = itemNew.title;
+        const newContentTitle = document.createTextNode(itemNew.title);
+        newTitle.appendChild(newContentTitle);
+        newItem.appendChild(newImage);
+        newItem.appendChild(newTitle);
+        contentList.appendChild(newItem);
+        const contentTitleMars = itemNew.title;
+          if (contentTitleMars.includes('Mars') || contentTitleMars.includes('Martians')){ 
+            newItem.classList.add('news__item--from-mars');
+            console.log(contentTitleMars);
       } 
-  }
-  console.log(data);
+      }
+          console.log(data); 
+});
 }
-
 renderNews();
-
