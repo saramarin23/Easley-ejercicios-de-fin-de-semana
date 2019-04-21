9f12630e16cea6edcd2c24c4b08494aa4d5f04cf
libertadpozos <libertad.pozos@gmail.com>
2019-04-21 14:06:38 +0200

    Add excercise 5

--- js/main.js
+++ js/main.js
@@ -1,38 +1,62 @@
 "use strict";
 
-// const data = [
-//   {
-//     title: "Asteroids 101",
-//     image: "https://via.placeholder.com/200x100"
-//   },
-//   {
-//     title: "Life on Mars",
-//     image: "https://via.placeholder.com/200x100"
-//   },
-//   {
-//     title: "Martians invade Earth",
-//     image: "https://via.placeholder.com/200x100"
-//   },
-//   {
-//     title: "Humans aren't real",
-//     image: "https://via.placeholder.com/200x100"
-//   },
-//   {
-//     title: "Space The Final Frontier",
-//     image: "https://via.placeholder.com/200x100"
-//   }
-// ];
+const data = [
+  {
+    title: "Asteroids 101",
+    image: "https://via.placeholder.com/200x100"
+  },
+  {
+    title: "Life on Mars",
+    image: "https://via.placeholder.com/200x100"
+  },
+  {
+    title: "Martians invade Earth",
+    image: "https://via.placeholder.com/200x100"
+  },
+  {
+    title: "Humans aren't real",
+    image: "https://via.placeholder.com/200x100"
+  },
+  {
+    title: "Space The Final Frontier",
+    image: "https://via.placeholder.com/200x100"
+  }
+];
 
-// //**** EJERCICIO 1*****
+// //**** EJERCICIO 4 *****
 // const ulEl = document.querySelector(".news");
 
+// function showImages(){
 // for (let i = 0; i < data.length; i++) {
-//   ulEl.innerHTML += `<li class="news__item">
+//   ulEl.innerHTML += `<li class="news__item news__item--no-image-visible'">
 //       <h2 class="news__title"> ${data[i].title}</h2>
 //       <img class="news__image" src=${data[i].image}
 //       alt=${data[i].title}>
 //     </li>`;
+  
+
 // }
+// }
+// showImages();
+
+
+
+//EJERCI0 4 FAIL 
+
+// function addListener(){
+//   const liEl=document.querySelectorAll('.news__item');
+// for (const list of liEl){
+//   list.addEventListener('click', addToggle);
+// }
+// }
+// function addToggle(event){
+//   const selectedImage= event.currentTarget;
+//   selectedImage.classList.toggle('news__item--no-image-visible');
+// }
+// addListener();
+// addToggle();
+
+
 
 // //**** EJERCICIO 2 *****
 
@@ -50,23 +74,66 @@
 
 //**** EJERCICIO 3 *****
 
-const ulEl = document.querySelector(".news");
+// const ulEl = document.querySelector(".news");
 
-function showNews(){
-  fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/news.json')
-  .then(response=> response.json())
-  .then(dataArrNews=>{
-      const arrNews = dataArrNews.news;
-      console.log(arrNews);
-         for (let i = 0; i < arrNews.length; i++) {
-           ulEl.innerHTML += `<li class="news__item">
-               <h2 class="news__title"> ${arrNews[i].title}</h2>
-               <img class="news__image" src=${arrNews[i].image}
-               alt=${arrNews[i].title}>
-             </li>`;
-         }
-    }   
-  )
+// function showNews(){
+//   fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/news.json')
+//   .then(response=> response.json())
+//   .then(dataArrNews=>{
+//       const arrNews = dataArrNews.news;
+//       console.log(arrNews);
+//          for (let i = 0; i < arrNews.length; i++) {
+//            ulEl.innerHTML += `<li class="news__item news__item--no-image-visible">
+//                <h2 class="news__title"> ${arrNews[i].title}</h2>
+//                <img class="news__image" src=${arrNews[i].image}
+//                alt=${arrNews[i].title}>
+//              </li>`;   
+//          }
+//     }   
+//   )
+// }
+// showNews();
+
+
+//Ejercicio 5
+let ulEl = document.querySelector(".news");
+
+// function createElements(){
+// fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palette.json')
+// .then(response=>response.json())
+// .then(data=>{
+//   const arrPalette= data.palettes;
+//   const arrColors= arrPalette[0].colors;
+//   console.log(arrColors);
+//   for(let i=0; i<arrColors.length; i++);
+//   //Creamos elemento nuevo
+//   const newItem=document.createElement('li');
+//   //Creamos contenido
+//   const newDiv=document.createElement('div');
+//   //añadimos atributo 
+//   newDiv.setAttribute('style', 'background-color:' + arrColors[i]);
+// //se lo añadimos a nuestro Li
+// newItem.appendChild(newDiv);
+// //añadir nuestro Li con todo a UL
+// ulEl.appendChild(newItem);
+// })
+// }
+// createElements();
+
+function createElements1(){
+fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palette.json')
+.then(response=> response.json())
+.then(data=>{
+  const palette=data.palettes;
+  const arrColors= palette[0].colors;
+  console.log(arrColors);
+for (let i=0; i<arrColors.length; i++){ 
+  ulEl.innerHTML += `<li class="new-item">
+               <div class="color-container" style="background-color:#${arrColors[i]}"> </div> </li>`;
+//                
 }
-//showNews();
+})
+}
+
+createElements1();
 
