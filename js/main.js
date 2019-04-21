7c21476853bf63dbf1d32d87f46ec33d93638816
LorenaBH <lorenabenitoherrero@gmail.com>
2019-04-21 13:22:17 +0200

    done exercice 2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 const contentList = document.querySelector('.news');
+const api = 'https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/news.json';
 
 const data = [
   {
@@ -26,21 +27,27 @@ const data = [
 
 function renderNews () {
   for(const itemNew of data) {
-  const newItem = document.createElement('li');
-  newItem.classList.add('news__item');
-  const newTitle = document.createElement('h2');
-  newTitle.classList.add('news__title');
-  const newImage = document.createElement('img');
-  newImage.classList.add('news__image');
-  newImage.src = itemNew.image;
-  newImage.alt = itemNew.title;
-  const newContentTitle = document.createTextNode(itemNew.title);
-  newTitle.appendChild(newContentTitle);
-  newItem.appendChild(newImage);
-  newItem.appendChild(newTitle);
-  contentList.appendChild(newItem);
+    const newItem = document.createElement('li');
+    newItem.classList.add('news__item');
+    const newTitle = document.createElement('h2');
+    newTitle.classList.add('news__title');
+    const newImage = document.createElement('img');
+    newImage.classList.add('news__image');
+    newImage.src = itemNew.image;
+    newImage.alt = itemNew.title;
+    const newContentTitle = document.createTextNode(itemNew.title);
+    newTitle.appendChild(newContentTitle);
+    newItem.appendChild(newImage);
+    newItem.appendChild(newTitle);
+    contentList.appendChild(newItem);
+    const contentTitleMars = itemNew.title;
+      if (contentTitleMars.includes('Mars') || contentTitleMars.includes('Martians')){ 
+        newItem.classList.add('news__item--from-mars');
+        console.log(contentTitleMars);
+      } 
   }
   console.log(data);
 }
 
 renderNews();
+
