5abb2addfbfca18ef7fb92e720e9be07aa5d011f
LorenaBH <lorenabenitoherrero@gmail.com>
2019-04-21 10:53:16 +0200

    done first exercise

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 'use strict';
+const contentList = document.querySelector('.news');
 
 const data = [
   {
@@ -22,3 +23,24 @@ const data = [
     image: 'https://via.placeholder.com/200x100'
   }
 ];
+
+function renderNews () {
+  for(const itemNew of data) {
+  const newItem = document.createElement('li');
+  newItem.classList.add('news__item');
+  const newTitle = document.createElement('h2');
+  newTitle.classList.add('news__title');
+  const newImage = document.createElement('img');
+  newImage.classList.add('news__image');
+  newImage.src = itemNew.image;
+  newImage.alt = itemNew.title;
+  const newContentTitle = document.createTextNode(itemNew.title);
+  newTitle.appendChild(newContentTitle);
+  newItem.appendChild(newImage);
+  newItem.appendChild(newTitle);
+  contentList.appendChild(newItem);
+  }
+  console.log(data);
+}
+
+renderNews();
