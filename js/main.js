e7fd2a2a26feadb83925d4e29a5485db2c8c9b06
carmenpmoreno <carmenp.moreno@gmail.com>
2019-04-22 10:33:37 +0200

    1/ Listado de noticias

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// ### 1/ Listado de noticias
+// Hay que pintar en el html un listado a partir del array:
 const data = [
   {
     title: 'Asteroids 101',
@@ -22,3 +24,39 @@ const data = [
     image: 'https://via.placeholder.com/200x100'
   }
 ];
+// En el ul del index hay que pintar cada objeto del siguiente modo:
+    // <li class="news__item">
+    //   <h2 class="news__title">AQUÍ-VA-EL-TÍTULO</h2>
+    //   <img class="news__image" src="AQUÍ-VA-LA-RUTA-DE-LA-IMAGEN" alt="AQUÍ-VA-EL-TÍTULO">
+    // </li>
+
+function pictureOnHtml () {
+  // constante para seleccionar la ul
+  const newsList = document.querySelector('.news');
+  // bucle para GENERAR un li por cada objeto del array
+  for (let i=0; i<data.length; i++) {
+    newsList.innerHTML += '<li class="news__item"></li>';
+  }
+  // // guardo en una constante cada li con Selector All, que me genera un array
+  const newsItem = document.querySelectorAll('.news__item')
+  // console.log(newsItem);
+  // con un bucle, recorro el array de lis y guardo en cada li el h2 y la img
+  for (let i=0; i<data.length; i++) {
+    newsItem[i].innerHTML += '<h2 class="news__title"></h2><img class="news__image" src="" alt="">';
+  }
+  // // // constantes para todos los h2 y img
+  const newsTitle = document.querySelectorAll('.news__title');
+  const newsImg = document.querySelectorAll('.news__image');
+  console.log(newsImg);
+  // // // // pintamos el primer objeto del array en el li
+  newsTitle[0].innerHTML = data[0].title;
+  newsImg[0].src = data[0].image;
+
+  // pintamos con un bucle los valores de todos los objetos en su correspondiente etiqueta de los lis
+  for (let i=0; i<data.length; i++) {
+    newsTitle[i].innerHTML += data[i].title;
+    newsImg[i].src = data[i].image;
+  }
+}
+pictureOnHtml()
+console.log(data);
