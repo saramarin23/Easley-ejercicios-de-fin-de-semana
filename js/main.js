768aa818c0001000a04ab033abe2137e0f76fee9
carmenpmoreno <carmenp.moreno@gmail.com>
2019-04-28 13:49:59 +0200

    improve 2/ Marte, el planeta rojo

--- js/main.js
+++ js/main.js
@@ -24,7 +24,6 @@ const data = [
     image: 'https://via.placeholder.com/200x100'
   }
 ];
-// 1/ Listado de noticias
 
 const newsList = document.querySelector('.news');
 for (let i = 0; i < data.length; i++) {
@@ -39,25 +38,18 @@ for (let i = 0; i < data.length; i++) {
   newsImg[i].src = data[i].image;
 }
 
-// 2/ Marte, el planeta rojo
+// 2/ Marte, el planeta rojo: destacar películas que tengan en su título relación con "Marte"
 
-// Una vez tenemos nuestra lista de noticias vamos a destacar los resultados "marcianos" aplicando la clase `.news__item--from-mars`.
-
-// Aplicaremos esta clase a los `<li>` cuyo título contenga “Mars” o “Martian”, para ello:
-
-  // 1. Buscaremos todos los elementos con clase `.news__item`.
   const newsItem = document.querySelectorAll('.news__item')
-// 2. Recorreremos la lista de elementos almacenando en una variable el contenido del título de cada elemento.
-for (let i=0; i<newsItem.length; i++) {
   const newsTitle = document.querySelectorAll('.news__title');
+
+for (let i=0; i<newsItem.length; i++) {
   let newsTitleContent = newsTitle[i].innerHTML;
-  // console.log(newsTitleContent.includes('Mars'));para comprobar si me miraba cuál tenía ese contenido y qué devolvía---> devuelve true si lo tiene
-  // 3. Usando el método `includes()` comprobaremos si contienen “Mars” o “Martian” y aplicaremos al `li`correspondiente la clase `.news__item--from-mars`
+  // console.log(newsTitleContent.includes('Mars')); ---> devuelve true o false
   if (newsTitleContent.includes('Mars')) {
     newsItem[i].classList.add('news__item--from-mars');
   }
   else if (newsTitleContent.includes('Martians')) {
     newsItem[i].classList.add('news__item--from-mars');
   }
-// 4. Tada!!
 }
