9109848bb37b1a31ca1f980a03a12b33e9872da9
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-08-06 16:38:25 +0200

    2. Marte, el planeta rojo

--- js/main.js
+++ js/main.js
@@ -40,3 +40,32 @@ function putList() {
 }
 
 putList();
+
+
+// Destacar los resultados "marcianos"
+
+//1. Buscamos todos los eltos con clase .news__item
+const newsItem = document.querySelectorAll('.news__item');
+
+//2. Recorremos la lista de eltos almacenando en una variable
+//el contenido del título de cada elto.
+/*
+* Función que recorre la lista de elementos del array data
+* y almacena el título en un array nuevo y lo devuelve
+*/
+function getTitle() {
+  let title = [];
+  for(let i=0; i<data.length; i++) {
+    title[i] = data[i].title;
+  }
+  //3. Usando el método includes() comprobaremos si contienen “Mars” o “Martian” y aplicaremos al li correspondiente la clase .news__item--from-mars
+  for(let i=0; i<title.length; i++) {
+    if(title[i].includes('Mars') || title[i].includes('Martian')) {
+      /* newsItem[i].classList.remove('news__item'); */
+      newsItem[i].classList.add('news__item--from-mars');
+    }
+  }
+  return title;
+}
+
+getTitle();
