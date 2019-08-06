6e672e1ba9c5043ada9e05793e9883e11d96621a
Constanza Sosa <constanza.sosa.g@gmail.com>
2019-08-06 18:25:22 +0200

    End 3. En el espacio nadie puede oir tus fetchs

--- js/main.js
+++ js/main.js
@@ -1,51 +1,82 @@
 'use strict';
 const list = document.querySelector('.news');
 
-const data = [
-  {
-    title: 'Asteroids 101',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Life on Mars',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Martians invade Earth',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Humans aren\'t real',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Space The Final Frontier',
-    image: 'https://via.placeholder.com/200x100'
-  }
-];
+// const data = [
+//   {
+//     title: 'Asteroids 101',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Life on Mars',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Martians invade Earth',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Humans aren\'t real',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Space The Final Frontier',
+//     image: 'https://via.placeholder.com/200x100'
+//   }
+// ];
 
-function writeData(){
-  for (let i=0; i<data.length; i++){
-    const newLi = document.createElement('li');
-    newLi.classList.add('news__item');
+// function writeData(){
+//   for (let i=0; i<data.length; i++){
+//     const newLi = document.createElement('li');
+//     newLi.classList.add('news__item');
 
-    const newTitle = document.createElement('h2');
-    newTitle.classList.add('news__title');
-    const newContent = document.createTextNode(data[i].title);
-    newTitle.appendChild(newContent);
+//     const newTitle = document.createElement('h2');
+//     newTitle.classList.add('news__title');
+//     const newContent = document.createTextNode(data[i].title);
+//     newTitle.appendChild(newContent);
 
-    const newImage = document.createElement('img');
-    newImage.classList.add('news__image');
-    newImage.setAttribute('src', data[i].image);
-    newImage.setAttribute('alt', data[i].title);
+//     const newImage = document.createElement('img');
+//     newImage.classList.add('news__image');
+//     newImage.setAttribute('src', data[i].image);
+//     newImage.setAttribute('alt', data[i].title);
 
-    newLi.appendChild(newTitle);
-    newLi.appendChild(newImage);
-    list.appendChild(newLi);
-  }
+//     newLi.appendChild(newTitle);
+//     newLi.appendChild(newImage);
+//     list.appendChild(newLi);
+//   }
+// }
+
+// writeData();
+
+const urlMars = 'https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/news.json';
+
+function getApi (){
+  fetch(urlMars)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      for (let i=0; i<data.news.length; i++){
+        const newLi = document.createElement('li');
+        newLi.classList.add('news__item');
+
+        const newTitle = document.createElement('h2');
+        newTitle.classList.add('news__title');
+        const newContent = document.createTextNode(data.news[i].title);
+        newTitle.appendChild(newContent);
+
+        const newImage = document.createElement('img');
+        newImage.classList.add('news__image');
+        newImage.setAttribute('src', data.news[i].image);
+        newImage.setAttribute('alt', data.news[i].title);
+
+        newLi.appendChild(newTitle);
+        newLi.appendChild(newImage);
+        list.appendChild(newLi);
+      }
+
+    });
 }
-
-writeData();
+getApi();
 
 const allItems = document.querySelectorAll('.news__item');
 let items = '';
