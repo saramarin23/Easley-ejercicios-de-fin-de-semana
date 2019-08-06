1b6288eecf9018af6c52ad98aa718758d898ad94
saramarin23 <saramarin.econ@gmail.com>
2019-08-06 23:51:51 +0200

    add exercise 1

--- js/main.js
+++ js/main.js
@@ -1,24 +1,46 @@
-'use strict';
+"use strict";
 
 const data = [
   {
-    title: 'Asteroids 101',
-    image: 'https://via.placeholder.com/200x100'
+    title: "Asteroids 101",
+    image: "https://via.placeholder.com/200x100"
   },
   {
-    title: 'Life on Mars',
-    image: 'https://via.placeholder.com/200x100'
+    title: "Life on Mars",
+    image: "https://via.placeholder.com/200x100"
   },
   {
-    title: 'Martians invade Earth',
-    image: 'https://via.placeholder.com/200x100'
+    title: "Martians invade Earth",
+    image: "https://via.placeholder.com/200x100"
   },
   {
-    title: 'Humans aren\'t real',
-    image: 'https://via.placeholder.com/200x100'
+    title: "Humans aren't real",
+    image: "https://via.placeholder.com/200x100"
   },
   {
-    title: 'Space The Final Frontier',
-    image: 'https://via.placeholder.com/200x100'
+    title: "Space The Final Frontier",
+    image: "https://via.placeholder.com/200x100"
   }
 ];
+
+const lists = document.querySelector(".news");
+
+lists.innerHTML += `<li class="news__item"><h2 class="news__title"><img class="news__image" src="${
+  data[0].image
+}" alt="${data[0].title}">${
+  data[0].title
+}</h2></li><li class="news__item"><h2 class="news__title"><img class="news__image" src="${
+  data[1].image
+}" alt="${data[1].title}">${
+  data[1].title
+}</h2></li><li class="news__item"><h2 class="news__title"><img class="news__image" src="${
+  data[2].image
+}" alt="${data[2].title}">${
+  data[2].title
+}</h2></li><li class="news__item"><h2 class="news__title"><img class="news__image" src="${
+  data[3].image
+}" alt="${data[3].title}">${
+  data[3].title
+}</h2></li><li class="news__item"><h2 class="news__title"><img class="news__image" src="${
+  data[4].image
+}" alt="${data[4].title}">${data[4].title}</h2></li>`;
