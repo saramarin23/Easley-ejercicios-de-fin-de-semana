39950638235738183adaf926dcf15eb1ac7c3f06
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-08-06 22:22:53 +0200

    Ejercicio 3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,41 @@
 'use strict';
 const news = document.querySelector('.news');
 
-const data = [
+function getBreeds() {
+  fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/news.json')
+    .then(response => response.json())
+    .then(data => {
+      for (const item of data.news) {
+        const newItem = document.createElement('li');
+        const newh2 = document.createElement('h2');
+        const newimg = document.createElement('img');
+        const newContent = document.createTextNode(item.title);
+        newItem.setAttribute('class', 'news__item');
+        newimg.setAttribute('class', 'news__image');
+        newimg.setAttribute('src', `${item.image}`);
+        newimg.setAttribute('alt', `${item.title} imagen`);
+        newh2.setAttribute('class', 'news__title');
+        newh2.appendChild(newContent);
+        newItem.appendChild(newh2);
+        newItem.appendChild(newimg);
+        news.appendChild(newItem);
+      }
+      const itemsElements = document.querySelectorAll('.news__item');
+      const classTitle = document.querySelectorAll('.news__title');
+
+      function resaltmarcianos() {
+        for (let i = 0; i < itemsElements.length; i++) {
+          if (classTitle[i].innerHTML.includes('Mars') | classTitle[i].innerHTML.includes('Martian')) {
+            itemsElements[i].classList.add('news__item--from-mars');
+          }
+        }
+      }
+      resaltmarcianos();
+    });
+}
+getBreeds();
+
+/*const data = [
   {
     title: 'Asteroids 101',
     image: 'https://via.placeholder.com/200x100'
@@ -22,8 +56,10 @@ const data = [
     title: 'Space The Final Frontier',
     image: 'https://via.placeholder.com/200x100'
   }
-];
-function addElementslist() {
+];*/
+
+
+/*function addElementslist() {
   for (const item of data) {
 
     const newItem = document.createElement('li');
@@ -41,19 +77,19 @@ function addElementslist() {
     news.appendChild(newItem);
   }
 }
-addElementslist();
-const itemsElements = document.querySelectorAll('.news__item');
-const classTitle = document.querySelectorAll('.news__title');
+addElementslist();*/
 
-function resaltmarcianos() {
+
+
+/*function resaltmarcianos() {
   for (let i = 0; i < itemsElements.length; i++) {
+    console.log('hola')
     if (classTitle[i].innerHTML.includes('Mars') | classTitle[i].innerHTML.includes('Martian')) {
       itemsElements[i].classList.add('news__item--from-mars');
     }
   }
 }
-
-resaltmarcianos();
+resaltmarcianos();*/
 
 /*for (const item of data){
   news.innerHTML += `<li class="news__item">
