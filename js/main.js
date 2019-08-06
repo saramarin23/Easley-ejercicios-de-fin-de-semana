a8ae7c8a216d1ce9bb4c4ef5988ba6c249e77728
Constanza Sosa <constanza.sosa.g@gmail.com>
2019-08-06 16:32:26 +0200

    End 1. Listado de noticias: list painted and styles in css

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 'use strict';
+const list = document.querySelector('.news');
 
 const data = [
   {
@@ -22,3 +23,26 @@ const data = [
     image: 'https://via.placeholder.com/200x100'
   }
 ];
+
+function writeData(){
+  for (let i=0; i<data.length; i++){
+    const newLi = document.createElement('li');
+    newLi.classList.add('news__item');
+
+    const newTitle = document.createElement('h2');
+    newTitle.classList.add('news__title');
+    const newContent = document.createTextNode(data[i].title);
+    newTitle.appendChild(newContent);
+
+    const newImage = document.createElement('img');
+    newImage.classList.add('news__image');
+    newImage.setAttribute('src', data[i].image);
+    newImage.setAttribute('alt', data[i].title);
+
+    newLi.appendChild(newTitle);
+    newLi.appendChild(newImage);
+    list.appendChild(newLi);
+  }
+}
+
+writeData();
