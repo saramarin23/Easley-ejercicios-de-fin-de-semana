15419dd93b77ccf7381e8d6a6009f16e894a0ac0
Migara <evamarcod@gmail.com>
2019-08-06 23:14:42 +0200

    Add exercise 1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const newUl = document.querySelector('.news');
 const data = [
   {
     title: 'Asteroids 101',
@@ -22,3 +23,28 @@ const data = [
     image: 'https://via.placeholder.com/200x100'
   }
 ];
+// <!--
+// Dentro de este ul hay que meter el contenido del array data
+// y cada elemento tiene que tener las siguientes clases:
+// <li class="news__item">
+//   <h2 class="news__title">AQUÍ-VA-EL-TÍTULO</h2>
+//   <img class="news__image" src="AQUÍ-VA-LA-RUTA-DE-LA-IMAGEN" alt="AQUÍ-VA-EL-TÍTULO">
+// </li>
+// -->
+
+for (let item of data){
+  const newimg = document.createElement('img');
+  newimg.setAttribute('src', item.image);
+  newimg.setAttribute('alt', item.title);
+  newimg.classList.add('news__image');
+  const newtitle = document.createElement('h2');
+  newtitle.classList.add('news__title');
+  const titletext = document.createTextNode(item.title);
+  newtitle.appendChild(titletext);
+  const newli = document.createElement('li');
+  newli.appendChild(newtitle);
+  newli.appendChild(newimg);
+  newli.classList.add('news__item');
+  newUl.appendChild(newli);
+
+}
